408f426962d588f31347676365b5a60abaa92ade
YaroslavKalinin <jaroslavkalinin97@gmail.com>
2025-06-14 18:53:21 +0300

    feat: add back button for navigation

--- client/app/containers/Navigation.tsx
+++ client/app/containers/Navigation.tsx
@@ -1,9 +1,30 @@
-import React from "react";
-import {useLaunchParams} from "@telegram-apps/sdk-react";
+import React, {useEffect} from "react";
+import {useLaunchParams, backButton} from "@telegram-apps/sdk-react";
 import {ThemeToggle} from "~/components";
+import {useLocation, useNavigate} from "react-router";
 
 const Navigation = () => {
-  const launchParams = useLaunchParams();
+  const launchParams = useLaunchParams()
+  const navigate = useNavigate()
+  const location = useLocation()
+  const canGoBack = location.key !== "default"
+
+  useEffect(() => {
+    if (canGoBack) {
+      if (!backButton.isMounted()) {
+        backButton.mount();
+        backButton.onClick(() => navigate(-1))
+      }
+      if (!backButton.isVisible()) {
+        backButton.show();
+      }
+    } else {
+      if (backButton.isMounted()) {
+        backButton.hide();
+      }
+    }
+  }, [canGoBack]);
+
   if (!launchParams?.tgWebAppData?.user) return null;
 
   return <div className="flex justify-between align-center">
--- client/app/create/create.tsx
+++ client/app/create/create.tsx
@@ -2,20 +2,25 @@ import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import TitleInput from "./TitleInput";
 import { Card, Button } from "../components";
+import Navigation from "~/containers/Navigation";
+import {Container} from "~/components";
 
 export default function Create() {
   const { t } = useTranslation("create");
   const [title, setTitle] = useState("");
 
   return (
-    <Card>
-      <div className="space-y-6">
-        <TitleInput value={title} onChange={setTitle} />
+    <Container>
+      <Navigation/>
+      <Card>
+        <div className="space-y-6">
+          <TitleInput value={title} onChange={setTitle} />
 
-        <Button disabled={!title.trim()} className="w-full">
-          {t("create")}
-        </Button>
-      </div>
-    </Card>
+          <Button disabled={!title.trim()} className="w-full">
+            {t("create")}
+          </Button>
+        </div>
+      </Card>
+    </Container>
   );
 }
