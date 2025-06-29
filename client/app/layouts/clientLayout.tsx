a91e36cf04fb1e2528d1d6298ce68f8406404a2b
YaroslavKalinin <jaroslavkalinin97@gmail.com>
2025-06-29 21:55:19 +0300

    fix: init for tma apps too

--- client/app/layouts/clientLayout.tsx
+++ client/app/layouts/clientLayout.tsx
@@ -11,7 +11,6 @@ export default function ClientLayout() {
   const initialize = async () => {
     if (!(await isTMA("complete"))) {
       mockEnv();
-      init();
     }
 
     if (import.meta.env.DEV) {
@@ -19,6 +18,7 @@ export default function ClientLayout() {
       eruda.default.init();
     }
 
+    init();
     setIsInitialized(true);
   };
 
